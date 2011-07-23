144e3cf63f2036a2a09045dda2aabcae948177bf
Ari Awan <ariawan.jp@gmail.com>
2011-07-23 20:35:01 +0900

    initiate site navigation

---
+++ js/boost.js
@@ -0,0 +1,39 @@
+(function($) {
+      
+var app = $.sammy('#main', function() {		
+	this.use('Template');
+	
+	this.get('#/', function(context) {
+		this.partial('templates/home.template');
+	});
+	
+	this.get('#/agenda', function(context) {
+		this.partial('templates/agenda.template');
+	});
+	
+	this.get('#/info', function(context) {
+		this.partial('templates/info.template');
+	});
+	
+	this.get('#/participants', function(context) {
+		this.partial('templates/participants.template');
+	});
+	
+	this.get('#/press', function(context) {
+		this.partial('templates/press.template');
+	});
+	
+	this.get('#/registration', function(context) {
+		this.partial('templates/registration.template');
+	});
+	
+	this.get('#/speakers', function(context) {
+		this.partial('templates/speakers.template');	
+	});
+});
+
+$(function() {
+	app.run('#/');
+});
+
+})(jQuery);
