e3be61d7ac6925dec0f01a355ea4d50905f1768e
Ari Awan <ariawan.jp@gmail.com>
2011-08-01 16:42:31 +0900

    replace jquery.scrollTo with regular js

--- js/boost.js
+++ js/boost.js
@@ -8,7 +8,8 @@ var app = $.sammy('#main', function() {
     	this.partial('templates/home.template');
     	this.load('data/speakers.json').renderEach('templates/home-speakers.template').appendTo('#speakers');
     	
-    	$.scrollTo('#main', 800);			     	
+    	var new_position = $('#main').offset();
+    	window.scrollTo(new_position.left,new_position.top);			     	
 	});
 	
 	this.get('#/agenda', function(context) {
@@ -16,32 +17,38 @@ var app = $.sammy('#main', function() {
 			$('#accordion1').accordion();
 			$('#accordion2').accordion();
 		});	
-		$.scrollTo('#main', 800);
+		var new_position = $('#main').offset();
+    	window.scrollTo(new_position.left,new_position.top);
 	});
 	
 	this.get('#/info', function(context) {
 		this.partial('templates/info.template');
-		$.scrollTo('#main', 800);
+		var new_position = $('#main').offset();
+    	window.scrollTo(new_position.left,new_position.top);
 	});
 	
 	this.get('#/participants', function(context) {
 		this.partial('templates/participants.template');
-		$.scrollTo('#main', 800);
+		var new_position = $('#main').offset();
+    	window.scrollTo(new_position.left,new_position.top);
 	});
 	
 	this.get('#/press', function(context) {
 		this.partial('templates/press.template');
-		$.scrollTo('#main', 800);
+		var new_position = $('#main').offset();
+    	window.scrollTo(new_position.left,new_position.top);
 	});
 	
 	this.get('#/registration', function(context) {
 		this.partial('templates/registration.template');
-		$.scrollTo('#main', 800);
+		var new_position = $('#main').offset();
+    	window.scrollTo(new_position.left,new_position.top);
 	});
 	
 	this.get('#/speakers', function(context) {
 		this.partial('templates/speakers.template');	
-		$.scrollTo('#main', 800);
+		var new_position = $('#main').offset();
+    	window.scrollTo(new_position.left,new_position.top);
 	});
 	
 });
