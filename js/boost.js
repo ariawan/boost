92f2c1062d6b4fa807bc30c5fbc73d644e2c86c1
Ari Awan <ariawan.jp@gmail.com>
2011-08-07 07:00:46 +0900

    added home navigation

--- js/boost.js
+++ js/boost.js
@@ -7,7 +7,7 @@ var app = $.sammy('#main', function() {
 	this.get('#/', function(context) {
     	this.partial('templates/home.template');
     	this.load('data/speakers.json').renderEach('templates/home-speakers.template').appendTo('#speakers');
-    	
+    	$('li a').removeClass('active');
     	var new_position = $('#main').offset();
     	window.scrollTo(new_position.left,new_position.top);			     	
 	});
@@ -16,37 +16,54 @@ var app = $.sammy('#main', function() {
 		this.partial('templates/agenda.template').then(function(){
 			$('#accordion1').accordion();
 			$('#accordion2').accordion();
+			$('li a').removeClass('active');
+			$('#nav_agenda').addClass('active');
 		});	
 		var new_position = $('#main').offset();
     	window.scrollTo(new_position.left,new_position.top);
 	});
 	
 	this.get('#/info', function(context) {
-		this.partial('templates/info.template');
+		this.partial('templates/info.template').then(function() {
+			$('li a').removeClass('active');
+			$('#nav_info').addClass('active');
+		});
 		var new_position = $('#main').offset();
     	window.scrollTo(new_position.left,new_position.top);
 	});
 	
 	this.get('#/participants', function(context) {
-		this.partial('templates/participants.template');
+		this.partial('templates/participants.template').then(function() {
+			$('li a').removeClass('active');
+			$('#nav_participants').addClass('active');
+		});
 		var new_position = $('#main').offset();
     	window.scrollTo(new_position.left,new_position.top);
 	});
 	
 	this.get('#/press', function(context) {
-		this.partial('templates/press.template');
+		this.partial('templates/press.template').then(function() {
+			$('li a').removeClass('active');
+			$('#nav_press').addClass('active');
+		});
 		var new_position = $('#main').offset();
     	window.scrollTo(new_position.left,new_position.top);
 	});
 	
 	this.get('#/registration', function(context) {
-		this.partial('templates/registration.template');
+		this.partial('templates/registration.template').then(function() {
+			$('li a').removeClass('active');
+			$('#nav_registration').addClass('active');
+		});
 		var new_position = $('#main').offset();
     	window.scrollTo(new_position.left,new_position.top);
 	});
 	
 	this.get('#/speakers', function(context) {
-		this.partial('templates/speakers.template');	
+		this.partial('templates/speakers.template').then(function() {
+			$('li a').removeClass('active');
+			$('#nav_speakers').addClass('active');
+		});	
 		var new_position = $('#main').offset();
     	window.scrollTo(new_position.left,new_position.top);
 	});
